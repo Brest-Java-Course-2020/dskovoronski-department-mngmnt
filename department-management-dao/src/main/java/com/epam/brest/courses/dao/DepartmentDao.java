e13154554ead234a73bbeaad6c984f54c2cd44b6
dmitry <dmitryskovoronski@gmail.com>
2020-02-14 02:11:07 -0500

    maven compile and build sucsess

---
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package main.java.com.epam.brest.courses.dao;
+
+import main.java.com.epam.brest.courses.model.Department;
+
+import java.util.List;
+public abstract class DepartmentDao {
+
+    abstract List<Department> getDepartments();
+
+    public static void main(String[] args) {
+
+    }
+
+}
