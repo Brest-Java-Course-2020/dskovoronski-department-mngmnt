978fe9a8bbe2892e6982cf1f822544e82708645b
dmitry <dmitryskovoronski@gmail.com>
2020-02-16 11:36:14 -0500

    mvn compile (interface DAO)

--- department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDao.java
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDao.java
@@ -3,12 +3,9 @@ package main.java.com.epam.brest.courses.dao;
 import main.java.com.epam.brest.courses.model.Department;
 
 import java.util.List;
-public abstract class DepartmentDao {
+public  interface DepartmentDao {
 
     abstract List<Department> getDepartments();
 
-    public static void main(String[] args) {
-
-    }
 
 }
